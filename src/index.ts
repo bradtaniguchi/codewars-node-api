985f0a3950a74b5e33fb7923b7bb5eee9e2c07bb
bradtaniguchi@gmail.com <bradtaniguchi@gmail.com>
2021-05-26 18:00:34 -0700

    Add empty type files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello world!');
+import * from './types/authored-challenge';
+import * from './types/code-challenge';
+import * from './types/completed-challenge';
+import * from './types/errors';
+import * from './types/user';
+
+
