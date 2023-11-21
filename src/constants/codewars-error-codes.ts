d02f82a56fb10c393785809aec6c91dfeea39da3
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-11-20 18:55:02 -0800

    docs: update codewars error link
    - Will expand with #70
    
    Close #63

--- src/constants/codewars-error-codes.ts
+++ src/constants/codewars-error-codes.ts
@@ -1,5 +1,7 @@
 /**
  * The array of http response error-codes the codewars api can return.
+ *
+ * @see CodewarsError
  */
 export const CODEWARS_ERROR_CODES = [
   400, 401, 403, 404, 405, 406, 422, 429, 500, 503
@@ -7,11 +9,16 @@ export const CODEWARS_ERROR_CODES = [
 
 /**
  * The type of http response codes the codewars api can return.
+ *
+ * @see CODEWARS_ERROR_CODES
  */
-export type CodewarsErrorCode = typeof CODEWARS_ERROR_CODES[number];
+export type CodewarsErrorCode = (typeof CODEWARS_ERROR_CODES)[number];
 
 /**
  * Type-guard that can be used to verify if the given code is a codewars http response codes.
+ *
+ * @see CODEWARS_ERROR_CODES
+ * @see CodewarsErrorCode
  */
 export const isCodewarsErrorCode = (code: unknown): code is CodewarsErrorCode =>
   typeof code === 'number' &&
