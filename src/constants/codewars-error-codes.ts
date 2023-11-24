6f4e868ab027c633ece51caa04ae7eae9b8eb656
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-11-24 12:31:37 -0800

    docs: update codewars-error-code file links

--- src/constants/codewars-error-codes.ts
+++ src/constants/codewars-error-codes.ts
@@ -1,7 +1,7 @@
 /**
  * The array of http response error-codes the codewars api can return.
  *
- * @see CodewarsError
+ * @see {@link CodewarsError}
  */
 export const CODEWARS_ERROR_CODES = [
   400, 401, 403, 404, 405, 406, 422, 429, 500, 503
@@ -10,15 +10,16 @@ export const CODEWARS_ERROR_CODES = [
 /**
  * The type of http response codes the codewars api can return.
  *
- * @see CODEWARS_ERROR_CODES
+ * @see {@link CODEWARS_ERROR_CODES}
  */
 export type CodewarsErrorCode = (typeof CODEWARS_ERROR_CODES)[number];
 
 /**
  * Type-guard that can be used to verify if the given code is a codewars http response codes.
  *
- * @see CODEWARS_ERROR_CODES
- * @see CodewarsErrorCode
+ * @param code the code to check, will also verify if a number
+ * @see {@link CODEWARS_ERROR_CODES}
+ * @see {@link CodewarsErrorCode}
  */
 export const isCodewarsErrorCode = (code: unknown): code is CodewarsErrorCode =>
   typeof code === 'number' &&
