efbc967fb61befb0e4d05de5b27dbda7b42c1e04
Brad <bradtaniguchi@gmail.com>
2024-06-19 17:16:19 -0700

    feat/70/review doc links (#77)
    * docs: update codewars-api tsdocs
    
    * docs: update codewars-error-code file links
    
    * feat: add freeze to CODEWARS_ERROR_CODES
    
    * feat: improve typing for webhook actions

--- src/constants/webhook-challenge-actions.ts
+++ src/constants/webhook-challenge-actions.ts
@@ -8,6 +8,7 @@
  * Further documentation is here:
  * https://dev.codewars.com/?shell#code-challenges
  *
+ * @see {@link WebhookChallengeActions}
  *
  */
 export const WEBHOOK_CHALLENGE_ACTIONS = Object.freeze([
@@ -24,6 +25,22 @@ export const WEBHOOK_CHALLENGE_ACTIONS = Object.freeze([
  * - approved - Code challenge was successfully approved (no longer in beta state)
  * - voted - Someone voted on the code challenge. Does not specify what type of vote
  * - solution_finalized - Someone submitted a solution to the code challenge
+ * @see {@link WEBHOOK_CHALLENGE_ACTIONS}
  */
 export type WebhookChallengeActions =
   (typeof WEBHOOK_CHALLENGE_ACTIONS)[number];
+
+/**
+ * Type-guard that can be used to verify if the given action is a webhook challenge action.
+ *
+ * @see {@link WEBHOOK_CHALLENGE_ACTIONS}
+ * @see {@link WebhookChallengeActions}
+ *
+ * @param action the action to check, will also verify if string
+ * @returns true if the given action is a webhook challenge action
+ */
+export const isWebhookChallengeAction = (
+  action: unknown
+): action is WebhookChallengeActions =>
+  typeof action === 'string' &&
+  WEBHOOK_CHALLENGE_ACTIONS.includes(action as WebhookChallengeActions);
